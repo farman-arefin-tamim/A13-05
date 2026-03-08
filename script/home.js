4da82ab63edcbb314a574c5d5cedb35192484e1f
farman-arefin-outlook <farman.arefin1@outlook.com>
2026-03-08 22:07:58 +0600

    Added buttons

--- script/home.js
+++ script/home.js
@@ -1,3 +1,13 @@
+let openData=[];
+let closedData=[];
+
+const allBtn=document.getElementById("all-btn");
+const openBtn=document.getElementById("open-btn");
+const closedBtn=document.getElementById("closed-btn");
+
+
+
+
 const loadData=()=>{
     const url="https://phi-lab-server.vercel.app/api/v1/lab/issues";
     fetch(url)
@@ -5,11 +15,65 @@ const loadData=()=>{
     .then(data=>displaydata(data.data));
 };
 
-// const status=(issue)=>{
-//     if(issue.status=="open"){
+let currentStates='all';
+
+// const toggleBtn=(id)=>{
+//     currentStates=id;
+//     allBtn.classList.remove('btn-primary');
+//     openBtn.classList.remove('btn-primary');
+//     closedBtn.classList.remove('btn-primary');
+
+//     allBtn.classList.add('btn');
+//     openBtn.classList.add('btn');
+//     closedBtn.classList.add('btn');
+
+//     const selected = document.getElementById(id);
+//     selected.classList.add('btn-primary');
+
+//     if(id=='all-btn'){
+//         displaydata();
+//     }else if(currentStates=='open-btn'){
 
 //     }
 // };
+
+function switchTab(id){
+    const tabs=["all","open","closed"];
+
+    for(const tab of tabs){
+        const tabName=document.getElementById(tab+"-btn");
+        console.log(tabName);
+        if(tab===id){
+            tabName.classList.add("btn-primary");
+        }else{
+            tabName.classList.remove("btn-primary");
+        }
+    }
+    
+};
+
+switchTab(currentStates);
+
+const statusImg=(issue)=>{
+    if(issue.status==="open"){
+        return './assets/Open-Status.png';
+    }else{
+        return './assets/Closed-Status.png';
+    }
+};
+
+//Priority Class
+const priorityClasses = {
+  high: "badge-error",
+  medium: "badge-warning",
+  low: "badge"
+};
+
+//Border Class
+const borderClass={
+    open: "border-green-700",
+    closed:"border-purple-700"
+};
 
 
 const displaydata=(issues)=>{
@@ -19,23 +83,23 @@ const displaydata=(issues)=>{
     for(const issue of issues){
         const card=document.createElement("div");
         card.innerHTML=`
-             <div class="card w-70 bg-base-100 card-sm shadow-sm border-t-4 border-green-700">
+             <div class="card w-70 h-70 bg-base-100 card-sm shadow-sm border-t-4 ${borderClass[issue.status]}">
                 <div class="card-body">
                      <div class="flex justify-between">
-                        <img src="./assets/${issue.status=='open'?'Open-status.png':'Closed-status.png'}" alt="">
-                        <div class="badge badge-soft font-light">Low</div>
+                        <img src="${statusImg(issue)}" alt="">
+                        <div class="badge badge-soft font-semibold ${priorityClasses[issue.priority]}">${issue.priority}</div>
                     </div>
-                    <h2 class="card-title">Fix Navigation Menu On Mobile Devices</h2>
-                    <p class="text-gray-400">The navigation menu doesn't collapse properly on mobile devices...</p>
+                    <h2 class="card-title">${issue.title}</h2>
+                    <p class="text-gray-400">${issue.description}</p>
 
                     <div>
-                        <div class="badge badge-soft badge-error">Error</div>
-                        <div class="badge badge-soft badge-warning">Warning</div>
+                        <div class="badge badge-soft badge-warning">${issue.labels[0]}</div>
+                        <div class="badge badge-soft badge-warning">${issue.labels[1]?issue.labels[1]:""}</div>
                     </div>
                     <hr class="text-gray-400">
                     <div class="text-gray-400">
-                        <p>#1 by john_doe</p>
-                        <p>1/15/2024</p>
+                        <p>#1 by ${issue.author}</p>
+                        <p>${new Date(issue.createdAt).toLocaleDateString()}</p>
                     </div>
                 </div>
             </div>
